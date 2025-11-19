7e8d270665b009d40f002dac755f25a4519577de
CovetingAphid3 <tadiwachwanda@gmail.com>
2025-11-19 22:48:04 +0200

    meta

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "@/contexts/ThemeContext";
-import Login from "@/components/Login";
 import Dashboard from "@/components/Dashboard";
 import Layout from "@/components/Layout";
 
@@ -9,8 +8,7 @@ function App() {
     <ThemeProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route
             path="/dashboard"
             element={
